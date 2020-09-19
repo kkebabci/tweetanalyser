67696b7715cd446ed26803d9738c8e205ec58ca3
kkebabci <kadir.bmh@gmail.com>
2020-09-19 22:54:37 +0300

    first commit

---
+++ src/main/java/com/kkebabci/tweetanalyser/TweetAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.kkebabci.tweetanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkebabci/tweetanalyser/TweetAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkebabci.tweetanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
